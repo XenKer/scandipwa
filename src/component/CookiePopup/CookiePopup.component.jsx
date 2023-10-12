a972f80d97d69b58e82744be9c884b4df596f33c
xen <kerasidisxenokratis@gmail.com>
2023-10-12 10:17:25 +0300

    CookiePopup styles extended for desktop

---
+++ src/component/CookiePopup/CookiePopup.component.jsx
@@ -0,0 +1,12 @@
+import {
+    CookiePopupComponent as SourceCookiePopupComponent,
+} from 'SourceComponent/CookiePopup/CookiePopup.component';
+
+import './CookiePopup.override.style';
+
+/** @namespace Scandipwaproject/Component/CookiePopup/Component */
+export class CookiePopupComponent extends SourceCookiePopupComponent {
+    // TODO implement logic
+}
+
+export default CookiePopupComponent;
